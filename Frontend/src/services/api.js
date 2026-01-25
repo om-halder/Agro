401abfcdfc041c2c215680aaaa021ec4f90423d8
om-halder <omhalder@outlook.com>
2026-01-26 00:09:28 +0530

    Add the backend url

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // replace with your backend URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api", // replace with your backend URL
 });
 
 // Attach Firebase token to every request
